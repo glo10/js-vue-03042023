5205f7fcaa2db604f4a224d1c5a4402aaf0929bd
Glodie Tshimini <contact@tshimini.fr>
2023-04-03 16:16:52 +0200

    feat: correction exercise 3 + update img exercise 4

---
+++ 0-exercices/corrections/exercice3/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <!--
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  -->
+  <MyPresentation/>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import MyPresentation from './components/MyPresentation.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    MyPresentation
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 0-exercices/corrections/exercice3/src/components/MyPresentation.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <main>
+      <section>
+        <h2>Saisie</h2>
+        <div>
+          <input type="text" placeholder="Nom" v-model="lastName">
+        </div>
+        <div>
+          <input type="text" placeholder="Prénom" v-model="firstName">
+        </div>
+        <div>
+          <input type="number" placeholder="Age" v-model="age">
+        </div>
+        <div>
+          <input v-model="techno" @keyup.enter="addTechno()" type="text" placeholder="Langage ou techno préférée" name="techno">
+        </div>
+        <div>
+          <button @click="addTechno" :disabled="techno.length === 0">Ajouter une technologie</button>
+        </div>
+      </section>
+      <section v-show="dataExist">
+        <h2>Affichage</h2>
+        <h3>Identité</h3>
+        <p>Nom : {{ lastName }}</p>
+        <p>Prénom : {{ firstName }}</p>
+        <p v-show="age != 0">{{ over18 }}</p>
+        <div v-show="technos.length > 0">
+          <h4>Technos préférées</h4>
+          <ul>
+            <li v-for="tech in technos" :key="tech">
+              {{ tech }}
+              <button @click="remove(tech)">x</button>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </main>
+  </div>
+</template>
+<script>
+export default {
+  name : 'MyPresentation',
+  data() {
+    return {
+      firstName : '',
+      lastName: '',
+      age: 0,
+      techno: '',
+      technos : []
+    }
+  },
+  computed: {
+    over18(){
+      if(this.age === 0) return ''
+      return this.age >= 18 ? 'Vous êtes majeur(e)': 'Vous êtes mineur(e)'
+    },
+    dataExist () {
+      return this.firstName.length > 0 || this.lastName.length > 0 || this.age != 0 || this.technos.length > 0
+    }
+  },
+  methods: {
+    addTechno() {
+      this.technos.push(this.techno)
+      this.techno = ''
+    },
+    remove(name) {
+      this.technos = this.technos.filter(techno => techno !== name)
+    }
+  }
+}
+</script>
